947f44136ecdaf99b552978287f9053c25ea6070
Sabian Pugh <sabian994@gmail.com>
2018-09-27 14:15:00 -0500

    added AppEngine class

---
+++ src/jm/edu/utech/ap/week5/AppEngine.java
@@ -0,0 +1,10 @@
+package jm.edu.utech.ap.week5;
+
+public class AppEngine {
+
+	public static void main(String[] args) {
+		Driveable driver = new Driver();
+		driver.drive(50);
+	}
+
+}
--- src/jm/edu/utech/ap/week5/Driver.java
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -1,5 +1,9 @@
 package jm.edu.utech.ap.week5;
 
-public class Driver {
-
+public class Driver implements Driveable 
+{
+	public void drive(float numberofMiles)
+	{
+		System.out.println(String.format("V_BOSS has DRIVEN " + "%.2f miles", numberofMiles));
+	}
 }
